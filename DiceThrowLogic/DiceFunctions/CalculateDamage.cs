9efb357ac08bdc43f3eff1922c0eceadaeab18cb
Winter Thane <saso.basic.2012@gmail.com>
2022-10-23 22:03:31 +0200

    die throw now shows picture

--- DiceThrowLogic/DiceFunctions/CalculateDamage.cs
+++ DiceThrowLogic/DiceFunctions/CalculateDamage.cs
@@ -9,5 +9,53 @@
 
             return damage;
         }
+
+        public static int ThrowDie(string die)
+        {
+            int result = 0;
+            Random random = new();
+
+            switch (die)
+            {
+                case "1d4":
+                    result = random.Next(1, 4 + 1);
+                    break;
+                case "1d6":
+                    result = random.Next(1, 6 + 1);
+                    break;
+                case "1d8":
+                    result = random.Next(1, 8 + 1);
+                    break;
+                case "1d10":
+                    result = random.Next(1, 10 + 1);
+                    break;
+                case "1d12":
+                    result = random.Next(1, 12 + 1);
+                    break;
+                case "2d4":
+                    result = random.Next(2, 4 + 1);
+                    break;
+                case "2d6":
+                    result = random.Next(2, 6 + 1);
+                    break;
+            }    
+
+            return result;
+        }
+
+        public static string ThrowDie(int die)
+        {
+            string result = die switch
+            {
+                1 => "diceone.png",
+                2 => "dicetwo.png",
+                3 => "dicethree.png",
+                4 => "dicefour.png",
+                5 => "dicefive.png",
+                6 => "dicesix.png",
+                _ => "diceone.png",
+            };
+            return result;
+        }
     }
 }
--- DiceThrowMAUI/MainPage.xaml.cs
+++ DiceThrowMAUI/MainPage.xaml.cs
@@ -10,14 +10,16 @@ public partial class MainPage : ContentPage
 	public Player MainPlayer { get; set; }
 	public EnemyRat Rat { get; set; }
 	public string CombatTextAll { get; set; }
+	public ImageSource DieRollPic { get; set; }
 
-	public MainPage()
+    public MainPage()
 	{
         InitializeComponent();
 		InitiatePlayer();
 		InitiateEnemy();
 
         CombatText.ListOfTextRows = new List<string>();
+		DieRollPic = "diceone.png";
 
 		BindingContext = this;
 	}
@@ -37,13 +39,16 @@ public partial class MainPage : ContentPage
 
 	private async void PlayerThrow_Clicked(object sender, EventArgs e)
 	{
-        var damage = CalculateDamage.MakeDamage(MainPlayer.MinDamage, MainPlayer.MaxDamage);
+		var die = CalculateDamage.ThrowDie("1d6");
+		PlayerDiceRollLabelText.Text = $"You rolled: " + die.ToString();
+		CombatTextAll = CombatText.CombatTextResult(PlayerDiceRollLabelText.Text);
 
+        var damage = CalculateDamage.MakeDamage(MainPlayer.MinDamage + die, MainPlayer.MaxDamage + die);
         PlayerDamageLabelText.Text = $"You hit {Rat.Name} for " + damage.ToString() + " damage.";
         CombatTextAll = CombatText.CombatTextResult(PlayerDamageLabelText.Text);
 		PlayerTurn = false;
 		_ = RotateSword("left");
-        ChangeTurn();
+        ChangeTurn(die);
 
 		PeriodicTimer timer = new(TimeSpan.FromSeconds(Config.ENEMY_TURN));
 		while (await timer.WaitForNextTickAsync())
@@ -52,6 +57,7 @@ public partial class MainPage : ContentPage
             timer.Dispose();
         }
 
+		SemanticScreenReader.Announce(PlayerDiceRollLabelText.Text);
         SemanticScreenReader.Announce(PlayerDamageLabelText.Text);
     }
 
@@ -63,25 +69,30 @@ public partial class MainPage : ContentPage
         CombatTextAll = CombatText.CombatTextResult(EnemyDamageLabelText.Text);
         PlayerTurn = true;
         _ = RotateSword("right");
-        ChangeTurn();
+        ChangeTurn(0);
 
         SemanticScreenReader.Announce(EnemyDamageLabelText.Text);
     }
 
-	private void ChangeTurn()
+	private void ChangeTurn(int die)
 	{
 		if (PlayerTurn)
 		{
-			PlayerThrowBtn.IsVisible = true;
+            DiceMiddlePic.IsVisible = false;
+            PlayerThrowBtn.IsVisible = true;
             PlayerDamageLabelText.IsVisible = false;
+			PlayerDiceRollLabelText.IsVisible = false;
             EnemyDamageLabelText.IsVisible = true;
 			PlayerFrame.BackgroundColor = new Color(50, 50, 50, 255);
             EnemyFrame.BackgroundColor = new Color(0, 0, 0, 255);
         }
 		else
 		{
-			PlayerThrowBtn.IsVisible = false;
-			PlayerDamageLabelText.IsVisible = true;            
+			DiceMiddlePic.Source = CalculateDamage.ThrowDie(die);
+            DiceMiddlePic.IsVisible = true;
+            PlayerThrowBtn.IsVisible = false;
+			PlayerDamageLabelText.IsVisible = true;
+			PlayerDiceRollLabelText.IsVisible = true;
 			EnemyDamageLabelText.IsVisible = false;
             EnemyFrame.BackgroundColor = new Color(50, 50, 50, 255);
             PlayerFrame.BackgroundColor = new Color(0, 0, 0, 255);
